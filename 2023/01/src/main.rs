1cf0280a89ae9efac128f125db903e1703a73f4f
Johnathan Davis <davisjc@users.noreply.github.com>
2023-12-01 22:44:58 -0800

    solve problem 1, part 2

--- 2023/01/src/main.rs
+++ 2023/01/src/main.rs
@@ -1,8 +1,32 @@
 use std::io;
 
-fn extract_digit(c: char) -> Option<u64> {
-    if let Some(digit) = c.to_digit(10) {
+static DIGIT_WORD_PAIRS: [(u64, &str); 9] = [
+    (1, "one"),
+    (2, "two"),
+    (3, "three"),
+    (4, "four"),
+    (5, "five"),
+    (6, "six"),
+    (7, "seven"),
+    (8, "eight"),
+    (9, "nine"),
+];
+
+fn digit_from_word(candidate: &str) -> Option<u64> {
+    for (digit, word) in DIGIT_WORD_PAIRS {
+        if candidate.starts_with(word) {
+            return Some(digit);
+        }
+    }
+    return None;
+}
+
+fn extract_digit(candidate_first: char, candidate: &str) -> Option<u64> {
+    if let Some(digit) = candidate_first.to_digit(10) {
         return Some(u64::from(digit));
+    }
+    if let Some(digit) = digit_from_word(candidate) {
+        return Some(digit);
     }
     return None;
 }
@@ -10,14 +34,14 @@ fn extract_digit(c: char) -> Option<u64> {
 fn main() {
     let mut total: u64 = 0;
     for line in io::stdin().lines().map(|l| l.unwrap()) {
-        for c in line.chars() {
-            if let Some(digit) = extract_digit(c) {
+        for (i, c) in line.char_indices() {
+            if let Some(digit) = extract_digit(c, &line[i..]) {
                 total += 10 * digit;
                 break;
             }
         }
-        for c in line.chars().rev() {
-            if let Some(digit) = extract_digit(c) {
+        for (i, c) in line.char_indices().rev() {
+            if let Some(digit) = extract_digit(c, &line[i..]) {
                 total += digit;
                 break;
             }
