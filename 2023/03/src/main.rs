645d29869d40aad5dcb2610be53ce4647eb2154b
Johnathan Davis <davisjc@users.noreply.github.com>
2023-12-03 20:28:22 -0800

    solve problem 3, part 2

--- 2023/03/src/main.rs
+++ 2023/03/src/main.rs
@@ -1,10 +1,11 @@
 use std::io;
+use std::collections::HashMap;
 use regex::Regex;
 
 fn main() {
-    let mut total: u64 = 0;
     let number_pattern = Regex::new(r"\d+").unwrap();
-    let symbol_pattern = Regex::new(r"[^\d\.]").unwrap();
+    let gear_pattern = Regex::new(r"\*").unwrap();
+    let mut nums_by_gear_pos = HashMap::new();
     let lines: Vec<String> = io::stdin().lines().map(|l| l.unwrap()).collect();
     for (row, line) in lines.iter().enumerate() {
         for num_match in number_pattern.find_iter(&line) {
@@ -24,28 +25,41 @@ fn main() {
             let mut candidates = Vec::new();
             if row > 0 {
                 let above = &lines[row - 1][search_start_col..=search_end_col];
-                candidates.push(above);
+                candidates.push((row - 1, search_start_col, above));
             }
             if start_col > 0 {
                 let left = &line[search_start_col..=search_start_col];
-                candidates.push(left);
+                candidates.push((row, search_start_col, left));
             }
             if end_col + 1 < line.len() {
                 let right = &line[search_end_col..=search_end_col];
-                candidates.push(right);
+                candidates.push((row, search_end_col, right));
             }
             if row + 1 < lines.len() {
                 let below = &lines[row + 1][search_start_col..=search_end_col];
-                candidates.push(below);
+                candidates.push((row + 1, search_start_col, below));
             }
-            for candidate in candidates.iter() {
-                if let Some(_) = symbol_pattern.find(&candidate) {
-                    let num = num_match.as_str().parse::<u64>().unwrap();
-                    total += num;
-                    break;
+            for (gear_row, gear_start_col, candidate) in candidates.iter() {
+                for gear_match in gear_pattern.find_iter(&candidate) {
+                    let gear_row = *gear_row;
+                    let gear_col = *gear_start_col + gear_match.start();
+                    let gear_pos = (gear_row, gear_col);
+                    let num = num_match.as_str().parse::<u32>().unwrap();
+                    if !nums_by_gear_pos.contains_key(&gear_pos) {
+                        nums_by_gear_pos.insert(gear_pos, Vec::new());
+                    }
+                    let nums = nums_by_gear_pos.get_mut(&gear_pos).unwrap();
+                    nums.push(num);
                 }
             }
         }
+    }
+    let mut total: u64 = 0;
+    for (_, nums) in &nums_by_gear_pos {
+        if nums.len() != 2 {
+            continue;
+        }
+        total += u64::from(nums[0] * nums[1]);
     }
     println!("{total}");
 }
