4da20b4d8782aff244c202414580775fb39032c8
Johnathan Davis <davisjc@users.noreply.github.com>
2023-12-05 17:54:18 -0800

    solve problem 5, part 2

--- 2023/05/src/main.rs
+++ 2023/05/src/main.rs
@@ -1,5 +1,6 @@
 use std::io;
 use std::cmp;
+use std::ops::Range;
 
 enum ParseState {
     ReadSeeds,
@@ -28,9 +29,17 @@ fn extract_nums(s: &str) -> Vec<i64> {
     s.split_whitespace().filter_map(|n| n.parse::<i64>().ok()).collect()
 }
 
-fn extract_seeds(line: &str) -> Vec<i64> {
+fn extract_seeds(line: &str) -> Vec<Range<i64>> {
 
-    extract_nums(&line[7..])
+    let nums = extract_nums(&line[7..]);
+    let mut seed_ranges = Vec::new();
+    let mut iter = nums.iter().peekable();
+    while iter.peek().is_some() {
+        let seed_start = *iter.next().unwrap();
+        let seed_end = seed_start + *iter.next().unwrap();
+        seed_ranges.push(seed_start..seed_end);
+    }
+    seed_ranges
 }
 
 fn extract_mapping(line: &str) -> Mapping {
@@ -47,12 +56,12 @@ fn extract_mapping(line: &str) -> Mapping {
 
 fn main() {
     let mut state = ParseState::ReadSeeds;
-    let mut seeds = None;
+    let mut seed_ranges = None;
     let mut transformations: Vec<Vec<Mapping>> = Vec::new();
     for line in io::stdin().lines().map(|l| l.unwrap()) {
         match state {
             ParseState::ReadSeeds => {
-                seeds = Some(extract_seeds(&line));
+                seed_ranges = Some(extract_seeds(&line));
                 state = ParseState::PrepareNextMap;
             },
             ParseState::PrepareNextMap => {
@@ -73,17 +82,19 @@ fn main() {
         }
     }
     let mut lowest_location = i64::MAX;
-    for seed in seeds.unwrap().into_iter() {
-        let mut val = seed;
-        for transformation in transformations.iter() {
-            for mapping in transformation.iter() {
-                if let Some(resolved) = mapping.resolve(val) {
-                    val = resolved;
-                    break;
+    for seed_range in seed_ranges.unwrap().into_iter() {
+        for seed in seed_range {
+            let mut val = seed;
+            for transformation in transformations.iter() {
+                for mapping in transformation.iter() {
+                    if let Some(resolved) = mapping.resolve(val) {
+                        val = resolved;
+                        break;
+                    }
                 }
             }
+            lowest_location = cmp::min(val, lowest_location);
         }
-        lowest_location = cmp::min(val, lowest_location);
     }
     println!("{lowest_location}");
 }
