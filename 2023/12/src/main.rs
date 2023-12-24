e59235ef259f308de4974a7d9062cc049f88fdaf
Johnathan Davis <davisjc@users.noreply.github.com>
2023-12-23 18:57:47 -0800

    solve problem 12, part 2

--- 2023/12/src/main.rs
+++ 2023/12/src/main.rs
@@ -1,16 +1,28 @@
 use std::io;
+use std::iter;
+use memoize::memoize;
 
 fn parse_line(line: String) -> (Vec<char>, Vec<usize>) {
     let (springs, groupings) = line.split_once(' ').unwrap();
-    let groupings = groupings
+    let springs = iter::repeat(springs)
+        .take(5)
+        .collect::<Vec<&str>>()
+        .join("?")
+        .chars()
+        .collect();
+    let groupings = iter::repeat(groupings)
+        .take(5)
+        .collect::<Vec<&str>>()
+        .join(",")
         .split(',')
         .filter_map(|n| n.parse().ok())
         .collect();
-    (springs.chars().collect(), groupings)
+    (springs, groupings)
 }
 
+#[memoize]
 fn count_arrangements(
-    springs: &[char], groupings: &[usize], cur_count: usize
+    springs: Vec<char>, groupings: Vec<usize>, cur_count: usize
 ) -> usize {
     if springs.is_empty() {
         if groupings.is_empty() {
@@ -34,16 +46,20 @@ fn count_arrangements(
 
     let mut total = 0;
     // try '#'
-    if springs[0] == '#' || springs[0] == '?' && !groupings.is_empty() {
-        total += count_arrangements(&springs[1..], groupings, cur_count + 1);
+    if springs[0] == '#' || springs[0] == '?' && !groupings.is_empty() &&
+       cur_count < groupings[0] {
+        total += count_arrangements(springs[1..].to_vec(), groupings.clone(),
+                                    cur_count + 1);
     }
     // try '.'
     if springs[0] == '.' || springs[0] == '?' {
         if cur_count > 0 && !groupings.is_empty() && cur_count == groupings[0] {
-            total += count_arrangements(&springs[1..], &groupings[1..], 0);
+            total += count_arrangements(springs[1..].to_vec(),
+                                        groupings[1..].to_vec(), 0);
         }
         if cur_count == 0 {
-            total += count_arrangements(&springs[1..], groupings, 0);
+            total += count_arrangements(springs[1..].to_vec(),
+                                        groupings.clone(), 0);
         }
     }
     return total;
@@ -53,7 +69,7 @@ fn main() {
     let mut total = 0;
     for line in io::stdin().lines().map(|l| l.unwrap()) {
         let (springs, groupings) = parse_line(line);
-        total += count_arrangements(&springs[..], &groupings[..], 0);
+        total += count_arrangements(springs, groupings, 0);
     }
     println!("{total}");
 }
