4da29caa328dd0b94db7afd71a3dc0923645e1c0
Johnathan Davis <davisjc@users.noreply.github.com>
2023-12-26 15:50:21 -0800

    solve problem 13, part 2

--- 2023/13/src/main.rs
+++ 2023/13/src/main.rs
@@ -7,11 +7,16 @@ struct Pattern {
     cols: Vec<Vec<bool>>,
 }
 
-fn find_reflection_idx<T: PartialEq>(v: &Vec<T>) -> Option<usize> {
+fn find_reflection_idx<T: PartialEq>(
+    v: &Vec<T>, skip: Option<usize>
+) -> Option<usize> {
     for mid in 1..v.len() {
         let low = if mid > v.len() / 2 { mid - (v.len() - mid) } else { 0 };
         let high = if mid <= v.len() / 2 { mid * 2 - 1 } else { v.len() - 1 };
         if v[low..mid].iter().eq(v[mid..=high].iter().rev()) {
+            if skip.is_some() && skip.unwrap() == mid {
+                continue;
+            }
             return Some(mid);
         }
     }
@@ -46,23 +51,54 @@ impl Pattern {
         patterns
     }
 
-    fn get_incidence(&self) -> Option<usize> {
-        let mut total = None;
-        if let Some(idx) = find_reflection_idx(&self.cols) {
-            total = Some(total.unwrap_or_default() + idx);
-        }
-        if let Some(idx) = find_reflection_idx(&self.rows) {
-            total = Some(total.unwrap_or_default() + 100 * idx);
-        }
-        total
+    fn get_incidence(
+        &self, skip_row: Option<usize>, skip_col: Option<usize>
+    ) -> (Option<usize>, Option<usize>) {
+        (find_reflection_idx(&self.rows, skip_row),
+         find_reflection_idx(&self.cols, skip_col))
     }
+
+    fn flip(&mut self, row: usize, col: usize) {
+        self.rows[row][col] = !self.rows[row][col];
+        self.cols[col][row] = !self.cols[col][row];
+    }
+}
+
+fn get_next_incidence(
+    pattern: &mut Pattern, original_row: Option<usize>,
+    original_col: Option<usize>
+) -> (Option<usize>, Option<usize>) {
+    for row in 0..pattern.rows.len() {
+        for col in 0..pattern.cols.len() {
+            pattern.flip(row, col);
+            let (next_row, next_col) = pattern.get_incidence(original_row,
+                                                             original_col);
+            pattern.flip(row, col);
+            if next_row.is_some()  {
+                return (next_row, None);
+            }
+            if next_col.is_some() {
+                return (None, next_col);
+            }
+        }
+    }
+    panic!("could not find incidence value in pattern: {pattern:?}");
 }
 
 fn main() {
     let mut total = 0;
     let patterns = Pattern::parse(io::stdin());
-    for pattern in patterns {
-        total += pattern.get_incidence().unwrap();
+    for mut pattern in patterns {
+        let (original_row, original_col) = pattern.get_incidence(None, None);
+        let (next_row, next_col) = get_next_incidence(&mut pattern,
+                                                      original_row,
+                                                      original_col);
+        if let Some(row) = next_row {
+            total += 100 * row;
+        }
+        if let Some(col) = next_col {
+            total += col;
+        }
     }
     println!("{total}");
 }
